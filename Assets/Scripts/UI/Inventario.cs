a25e4b7424642493a5f4be3f21ee7a5502487fb5
paulacuencaa <paula.cuenca_5426@escuelaces.net>
2023-05-18 21:09:27 +0200

    empezando el inventario

---
+++ Assets/Scripts/UI/Inventario.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+    bool inventarioEnabled;
+    [SerializeField] GameObject inventario;
+    int allSlots;
+    int enabledSlots;
+    GameObject[] slots;
+    [SerializeField] GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = slotHolder.transform.childCount;
+        slots = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventarioEnabled = !inventarioEnabled;
+        }
+        if (inventarioEnabled==true)
+        {
+            Debug.Log("Activo");
+            inventario.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("inactivo");
+            inventario.SetActive(false);
+        }
+    }
+}
