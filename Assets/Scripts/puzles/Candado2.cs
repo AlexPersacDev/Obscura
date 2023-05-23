a7c6a5481532712c6b528661d42dcaf4b3992dea
paulacuencaa <paula.cuenca_5426@escuelaces.net>
2023-05-23 10:53:58 +0200

    candado 2 principio

---
+++ Assets/Scripts/puzles/Candado2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Candado2 : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera playerLook;
+    [SerializeField] CinemachineVirtualCamera zoomCandado2;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
