3cecba6e44518c4eb9c6afdd405a71e83422e5e4
paulacuencaa <paula.cuenca_5426@escuelaces.net>
2023-05-25 13:41:24 +0200

    Candado 2 terminado suuu

--- Assets/Scripts/puzles/Candado 2/Candado2.cs
+++ Assets/Scripts/puzles/Candado 2/Candado2.cs
@@ -15,14 +15,23 @@ public class Candado2 : MonoBehaviour
     [SerializeField] GameObject codigodrchMedio;
     [SerializeField] GameObject codigodrch;
 
+    [SerializeField] GameObject candado;
+    Animator animCandado;
+
     int[] arrayNumeros = new int[4]; 
     int contadorA = 0;
     int contadorB = 0;
     int contadorC = 0;
     int contadorD = 0;
+
     void Start()
     {
-       
+        arrayNumeros[0] = 6;
+        arrayNumeros[1] = 0;
+        arrayNumeros[2] = 8;
+        arrayNumeros[3] = 4;
+
+        animCandado = candado.GetComponent<Animator>();
     }
 
     
@@ -54,9 +63,9 @@ public class Candado2 : MonoBehaviour
     public void FlechaArribaIzqMedio()
     {
         contadorB++;
-        if (contadorB<0)
+        if (contadorB>9)
         {
-            contadorB = 9;
+            contadorB = 0;
         }
         ComprobarContrasenha();
         codigoIzqMedio.transform.Rotate(new Vector3(36, 0, 0));
@@ -64,26 +73,51 @@ public class Candado2 : MonoBehaviour
     public void FlechaAbajoIzqMedio()
     {
         contadorB--;
-        if (contadorB)
+        if (contadorB<0)
         {
-
+            contadorB = 9;
         }
+        ComprobarContrasenha();
         codigoIzqMedio.transform.Rotate(new Vector3(-36, 0, 0));
     }
     public void FlechArribaDrchMedio()
     {
+        contadorC++;
+        if (contadorC>9)
+        {
+            contadorC = 0;
+        }
+        ComprobarContrasenha();
         codigodrchMedio.transform.Rotate(new Vector3(36, 0, 0));
     }
     public void FlechAbajoDrchMedio()
     {
+        contadorC--;
+        if (contadorC<0)
+        {
+            contadorC = 9;
+        }
+        ComprobarContrasenha();
         codigodrchMedio.transform.Rotate(new Vector3(-36, 0, 0));
     }
     public void FlechArribaDrch()
     {
+        contadorD++;
+        if (contadorD>9)
+        {
+            contadorD = 0;
+        }
+        ComprobarContrasenha();
         codigodrch.transform.Rotate(new Vector3(36, 0, 0));
     }
     public void FlechAbajoDrch()
     {
+        contadorD--;
+        if (contadorD<0)
+        {
+            contadorD = 9;
+        }
+        ComprobarContrasenha();
         codigodrch.transform.Rotate(new Vector3(-36, 0, 0));
     }
 
@@ -93,6 +127,13 @@ public class Candado2 : MonoBehaviour
         if (arrayNumeros[0]== contadorA&&arrayNumeros[1]==contadorB&&arrayNumeros[2]==contadorC&& arrayNumeros[3]==contadorD)
         {
             //se abre el candado
+            Debug.Log("Has abierto el candado");
+            animCandado.SetBool("SeHaAbierto",true);
+            //candado.SetActive(false);
         }
     }
+    //public void EventoAnimacion()
+    //{
+    //    candado.SetActive(false);
+    //}
 }
