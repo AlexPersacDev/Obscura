9dc578851c04dd33fe40eaabc895caa6d2b3fe4c
Alexpersac <Alejandro.pereira_7439@escuelaces.net>
2023-05-30 10:59:14 +0200

    scripting interfaces

---
+++ Assets/ScriptableObjects/GameManagerSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="GameManager")]
+public class GameManagerSO : ScriptableObject
+{
+    [SerializeField] GameObject photo;
+}
